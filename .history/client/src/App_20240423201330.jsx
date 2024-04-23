019662f7b5fb5897ddf7cb8c1fdfdff8b598cb43
Artur <artgouveiag@gmail.com>
2024-04-23 20:18:52 +0100

    install react js and tailwindcss and create the first template

---
+++ .history/client/src/App_20240423201330.jsx
@@ -0,0 +1,10 @@
+function App() {  
+
+  return (  
+    <div>
+      <h1 className="text-3">App</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ .history/client/src/App_20240423201357.jsx
@@ -0,0 +1,10 @@
+function App() {  
+
+  return (  
+    <div>
+      <h1 className="text-3xl text">App</h1>
+    </div>
+  )
+}
+
+export default App
