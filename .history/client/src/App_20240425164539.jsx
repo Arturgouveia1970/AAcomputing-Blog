39d5758cc44efc98ed6dc18e2f5a0938b57b6fe7
Artur <artgouveiag@gmail.com>
2024-04-25 17:08:09 +0100

    created footer component

--- .history/client/src/App_20240425164539.jsx
+++ .history/client/src/App_20240425164539.jsx
@@ -6,7 +6,6 @@ import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 function App() {  
 
@@ -21,7 +20,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
-      <Footer ></Footer>
+      <
     </BrowserRouter>
   )
 }
---
+++ .history/client/src/components/Footer_20240425164524.jsx
@@ -0,0 +1,7 @@
+export default function Footert() {
+    return (
+      <div>
+        <h1 className="text-3xl text-red-500">Footert</h1>
+      </div>
+    )
+  }
